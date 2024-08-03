669cce2d97d0b6074ee2417dfd5b3fa766233d06
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-03 16:32:40 -0400

    Alt dev (#5)
    * updating
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * updating build
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * removing
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * triggering terraform build
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * adding compatiblity flags
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * adding node options
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * removing some pages for space
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * trying experimental minify
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * cleanup
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    * adding remotes
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const TerserPlugin = require("terser-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const ExternalRemotesPlugin = require("external-remotes-plugin");
+const packageJson = require("./package.json");
 
 const nextConfig = {
   reactStrictMode: true,
@@ -11,6 +13,38 @@ const nextConfig = {
     theme: "DEFAULT",
   },
   swcMinify: true,
+  webpack: (config, options) => {
+    config.plugins.push(
+      new ModuleFederationPlugin({
+        name: "container",
+        filename: "remoteEntry.js",
+        exposes: {},
+        remotes: {
+          dtk: "storybook@https://dtk.suncoast.systems/remoteEntry.js",
+        },
+        shared: {
+          ...packageJson.dependencies,
+          react: {
+            singleton: true,
+            requiredVersion: packageJson.dependencies.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: packageJson.dependencies["react-dom"],
+          },
+        },
+      })
+    );
+    config.plugins.push(new ExternalRemotesPlugin());
+
+
+    config.resolve.fallback = {
+      stream: require.resolve("stream-browserify"),
+    }
+
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
