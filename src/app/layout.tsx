5bf3f4ab596d78abac609a2bcb316b79693dc2a6
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-20 15:45:33 -0400

    starting with idea
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,13 +22,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={montserrat.className}>
-        <StyledComponentsRegistry>
-          <AppProvider>
-            <StyledContext>{children}</StyledContext>
-          </AppProvider>
-        </StyledComponentsRegistry>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+
+        <link rel="icon" href="/favicon.ico" sizes="32x32"></link>
+        <link rel="icon" href="/armada.svg" type="image/svg+xml"></link>
+        <link rel="apple-touch-icon" href="/armada-180.png"></link>
+        <link rel="manifest" href="/manifest.webmanifest"></link>
+
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Suncoast.systems Dev project"
+        />
+        <link rel="manifest" href="/manifest.json" ></link>
+
+        <title>Armada</title>
+      </head>
+
+
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-8MHBD6Z0FG"></script>
+
+      <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="root"></div>
+
       </body>
     </html>
   );
